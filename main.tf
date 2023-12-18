e8d58578b5e03c6ec0dc476286c3fa32053a3eb9
shyam2180 <106369527+shyam2180@users.noreply.github.com>
2023-12-18 16:36:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,31 +7,3 @@ terraform {
     }
   }
 }
-provider "databricks" {
-  host  = "https://dbc-ff4a8e7c-a0e4.cloud.databricks.com/"
-  token = "dapidbbacbbe2594b133eb06cf028d114f47"
-}
- 
-# Databricks workspace resource definition
-resource "databricks_workspace" "test" {
-  # Configure workspace settings
-  name = "/Repos/shyamkumarr@jmangroup.com/databricks/cicd"
-}
- 
-# Databricks cluster resource definition
-resource "databricks_cluster" "test" {
-  # Configure cluster settings
-  cluster_name            = "DE-cluster"
-  spark_version           = "13.3.x-scala2.12"
-  node_type_id            = "i3.xlarge"
-  num_workers             = 2
-  autotermination_minutes = 10
-}
- 
-# Databricks Notebook resource definition
-resource "databricks_notebook" "test_notebook" {
-  # Configure notebook settings
-  name    = "my_notebook"
-  path    = databricks_workspace.test.path
-  content = file("C:\\Users\\AravindaNarayana\\Desktop\\test_cicd_output\\ci_cd.py.txt")
-}
