bd67711812cf9f6c008b4f6b790c41b8f7d449fb
shyam2180 <106369527+shyam2180@users.noreply.github.com>
2023-12-19 10:17:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     databricks = {
       source  = "databricks/databricks"
-      version = "0.4.0"  # Specify the version you want to use
+      version = ">=0.4.0"  # Specify the version you want to use
     }
   }
 }
