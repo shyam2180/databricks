62c650f16e91740901fca075609288f5861f70d9
shyam2180 <106369527+shyam2180@users.noreply.github.com>
2023-12-20 15:48:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
-# Configure Databricks provider
 terraform {
   required_providers {
     databricks = {
       source  = "databricks/databricks"
-      version = ">=0.4.0"  # Specify the version you want to use
+      version = ">=0.4.0"
     }
   }
 }
@@ -12,17 +11,32 @@ provider "databricks" {
   token = "dapidbbacbbe2594b133eb06cf028d114f47"
 }
 
-resource "databricks" "main" {
-  # main workspace configuration
-  name = "main"
-  version = ">=0.4.0"
-}
-
-# Upload Python file to main workspace
-resource "null_resource" "upload_python_file" {
-  provisioner "local-exec" {
-    command = "databricks workspace import -l /Local/Path/To/Your/PythonScript.py /Workspace/Path/In/main"
+provider "azurerm" {
+  features {
+    managed_disk {
+      expand_without_downtime = true
+    }
   }
-
-  depends_on = [databricks_workspace.main]
 }
+# # Databricks cluster resource definition To create a cluster
+# resource "databricks_cluster" "main" {
+#   # Configure cluster settings
+#   cluster_name            = "DE-cluster"
+#   spark_version           = "13.3.x-scala2.12"
+#   node_type_id            = "i3.xlarge"
+#   num_workers             = 2
+#   autotermination_minutes = 10
+# }
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_databricks_workspace" "example" {
+  name                = "databricks-test"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "standard
+}
+ 
