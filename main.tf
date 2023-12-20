7223ac7c240033725d3e706abe8fea774ce1daf8
shyam2180 <106369527+shyam2180@users.noreply.github.com>
2023-12-20 15:59:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,5 +37,6 @@ resource "azurerm_databricks_workspace" "example" {
   name                = "databricks-test"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
+  sku                 = "standard"
 }
  
