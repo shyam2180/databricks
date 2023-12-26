f8845bbb35057397df72969a43361e190d348932
shyam2180 <shyam2180>
2023-12-26 06:43:39 +0000

    dev updated

---
+++ cicd/cicd.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# Import the pandas library as pd
+import pandas as pd
+
+# Dummy data
+data = {
+    'Name': ['John', 'Jane', 'Bob'],
+    'Age': [25, 30, 22],
+    'City': ['New York', 'San Francisco', 'Los Angeles']
+}
+
+# Create a DataFrame using pandas
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+#shyam
+print(df)
